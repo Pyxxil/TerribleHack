0649bfed7b8be5f7f5d8bc64adeb11407a83a868
Sunny Feng <45865118+sunnyfeng17@users.noreply.github.com>
2021-07-03 00:47:54 +1200

    add amazing search engine
    search which returns lmgtfy link

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ThermApp from "./thermapp";
+import LmgtfySearchEngine from "./LmgtfySearchEngine";
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Route path="/thermapp" component={ThermApp} />
+        <Route path="/lmgtfy-search-engine" component={LmgtfySearchEngine} />
       </Router>
     </div>
   );
---
+++ src/LmgtfySearchEngine/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Search from "./search";
+
+const LMGTFY = () => {
+    console.log("here")
+  return (
+    <div>
+        <Search />
+    </div>
+  );
+};
+
+export default LMGTFY;
