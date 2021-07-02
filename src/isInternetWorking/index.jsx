b065cfcaffdd697e81da4371650ea16e80151cd5
Mohammad Hassaan Mirza <41189871+mmir415@users.noreply.github.com>
2021-07-02 21:37:25 +1200

    Styiling the page

--- src/isInternetWorking/index.jsx
+++ src/isInternetWorking/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-const isInternetWorking = () =>{
 
+const isInternetWorking = () =>{
+const divStyle = {
+    color: 'blue',
+    fontSize: '200px',
+    margin: 0,
+    lineHeight: 0,
+}
     return(
+        <div>
+    <p>Is your internet working?</p>
+    <div style={divStyle}>
     <p>Yes</p>
+    </div>
+    </div>
     );
 }
 
