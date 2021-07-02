331a226bba3ce1cde83f5377008001a39d27e3cd
Josh Hill <github@pyxxilated.studio>
2021-07-02 20:31:00 +1200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import ThermApp from "./thermapp";
 
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/thermapp" component={ThermApp} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
