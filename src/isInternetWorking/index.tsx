1b77bc24671606b65f112e1b24be3fb0d434b07a
Kimberley <kim@evans-parker.com>
2021-07-04 13:17:34 +1200

    Design IsInternetWorking

--- src/isInternetWorking/index.tsx
+++ src/isInternetWorking/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 const divStyle = {
-  color: "blue",
+  color: "green",
   fontSize: "200px",
   margin: 0,
-  lineHeight: 0,
+  lineHeight: "200px",
+  padding: 0
 };
 
 const isInternetWorking = () => {
   return (
     <div>
-      <p>Is your internet working?</p>
-      <div style={divStyle}>
-        <p>Yes</p>
+      <div className="vertical-horizontal-center">
+        <h1>Is your internet working?</h1>
+        <p style={divStyle}>Yes</p>
       </div>
     </div>
   );
