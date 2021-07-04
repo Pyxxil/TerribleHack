6b2778e6c2eb85a860050d58f5f96dee86fd9138
Kimberley <kim@evans-parker.com>
2021-07-04 15:26:07 +1200

    Add nice ducky

--- src/CondescendingRubberDuck/index.tsx
+++ src/CondescendingRubberDuck/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Message from "./message";
-import Ducky from "./ducky.png";
 import "./styles.css";
 
 const CondescendingRubberDuck = () => {
   return (
     <div className="ducky">
-      <img src={Ducky} />
       <Message />
     </div>
   );
